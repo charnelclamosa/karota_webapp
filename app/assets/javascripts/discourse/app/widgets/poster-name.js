8397fc80cf86a68b94b15e9aaa84d0c8e6617053
Charnel <charnel@pez.ai>
2022-09-29 22:07:23 +0800

    Display credit balance as text beside the user avatar

--- app/assets/javascripts/discourse/app/widgets/poster-name.js
+++ app/assets/javascripts/discourse/app/widgets/poster-name.js
@@ -5,6 +5,7 @@ import getURL from "discourse-common/lib/get-url";
 import { h } from "virtual-dom";
 import { iconNode } from "discourse-common/lib/icon-library";
 import { prioritizeNameInUx } from "discourse/lib/settings";
+import RawHtml from "discourse/widgets/raw-html";
 
 let sanitizeName = function (name) {
   return name.toLowerCase().replace(/[\s\._-]/g, "");
@@ -109,7 +110,9 @@ export default createWidget("poster-name", {
     const classNames = nameFirst
       ? ["first", "full-name"]
       : ["first", "username"];
-
+    console.log("username", username)
+    console.log("name", name)
+    console.log("nameFirst", nameFirst)
     if (attrs.staff) {
       classNames.push("staff");
     }
@@ -125,6 +128,7 @@ export default createWidget("poster-name", {
     if (attrs.new_user) {
       classNames.push("new-user");
     }
+    console.log("classNames", classNames)
 
     const primaryGroupName = attrs.primary_group_name;
     if (primaryGroupName && primaryGroupName.length) {
@@ -141,8 +145,9 @@ export default createWidget("poster-name", {
 
     const afterNameContents =
       applyDecorators(this, "after-name", attrs, this.state) || [];
-
+    const credits = this.creditsIndicator(attrs);
     nameContents = nameContents.concat(afterNameContents);
+    nameContents = nameContents.concat(credits);
 
     const contents = [
       h("span", { className: classNames.join(" ") }, nameContents),
@@ -190,5 +195,17 @@ export default createWidget("poster-name", {
     if (attrs.user && attrs.user.status) {
       contents.push(this.attach("post-user-status", attrs.user.status));
     }
+  },
+
+  creditsIndicator(attrs) {
+    const contents = [];
+    if (attrs.userCustomFields && attrs.userCustomFields.credit_balance) {
+      const credits = attrs.userCustomFields.credit_balance
+      let html = new RawHtml({
+        html: `<span style="margin-left: 5px;">${credits} <-- credit balance</span>`
+      })
+      contents.push(html)
+    }
+    return contents;
   },
 });
